2d1a58fde682e05e8b01f4b1ce0f110d71748135
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2023-07-24 23:06:41 -0500

    ImageUpload Front End

--- vite-project/src/components/ImageUpload.tsx
+++ vite-project/src/components/ImageUpload.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import '../scss/pages/imageup.scss'
 
 export const ImageUpload = () => {
+
+  const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dwzjh0asp/image/upload'
+  const CLOUDINARY_UPLOAD_PRESET = 'llekbdwp'
+
+
+  const imgUploader = async (e: any) => {
+    const file = e.target.files[0].name
+
+    // This formData will be sent to the server
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+
+    // Send the formData to the server
+    const res = await fetch(CLOUDINARY_URL, {
+      method: 'POST',
+      body: formData,
+      headers: { 'content-type': 'multipart/form-data' }
+    })
+    
+    console.log(res);
+    
+  }
+
   return (
-    <div>ImageUpload</div>
+    <main className="imageup-main">
+      <section className="imageup-container">
+        <img alt="holi" id="img-preview" />
+
+        <div className="imageup-footer">
+          <input 
+            type="file" 
+            id="img-uploader"
+            onChange={(e) => { imgUploader(e) }}
+          />
+          
+        </div>
+      </section>
+    </main>
   )
 }
